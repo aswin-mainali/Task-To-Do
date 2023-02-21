72ed3bb48a672084e77bc83939f761475b72c09b
Aswin Mainali <106871493+aswin-mainali@users.noreply.github.com>
2023-02-21 13:22:00 +0545

    Add files via upload

---
+++ todo-app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const todos = getSavedTodos();
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters);
+
+document.querySelector('#filter-todo').addEventListener('input', (e) => {
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters);
+})
+
+document.querySelector('#todo-form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const todoValue = e.target.elements.addTodo.value.trim();
+    if (todoValue.length > 0){
+        todos.push({
+        id: uuidv4(),
+        text: todoValue, 
+        completed: false});
+        saveTodos(todos);
+        renderTodos(todos, filters);
+        e.target.elements.addTodo.value='';
+    }
+})
+
+document.querySelector('#hide-completed').addEventListener('change', (e)=>{
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
+});
+
+
+let showhide=document.getElementById("hide-completed");
+showhide.addEventListener('click',function(e){
+    if(e.target.checked){
+        localStorage.setItem('showhide','true');
+    }
+    else{
+        
+        localStorage.setItem('showhide','false');
+    }
+})
+
+let isshowhide = localStorage.getItem('showhide');
+if(isshowhide=='true'){
+    showhide.checked = true;
+    filters.hideCompleted = true;
+    renderTodos(todos, filters);
+}
